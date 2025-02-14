eb1de919ada2ebe9fa5eb57acec52d91f90f4505
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2025-02-14 09:25:56 -0600

    Se agrego el codigo para mostrar el dia, el mes  y el anio de la  fecha actual

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ console.log(dateNow.toLocaleString());
 
 console.log(dateNow.toISOString().split("T")[0]);
 
+console.log(dateNow.getDate() + 1);
+
+console.log(dateNow.getFullYear());
+
+console.log(dateNow.getMonth());
+
 export function getDateNow() {
   let dateNow = new Date();
 
