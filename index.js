df9a62b24b560d95e9b2c76f5032aa9c80deb90a
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2025-02-14 09:35:32 -0600

    feat: Se agrego logica a una funcion para retornar la fecha actual en un solo formato por ahora

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
-let dateNow = new Date();
+let dateNow = new Date()
 
-console.log(dateNow);
+console.log(dateNow)
 
-console.log(dateNow.toLocaleDateString());
+console.log(dateNow.toLocaleDateString())
 
-console.log(dateNow.toLocaleString());
+console.log(dateNow.toLocaleString())
 
-console.log(dateNow.toISOString().split("T")[0]);
+console.log(dateNow.toISOString().split("T")[0])
 
-console.log(dateNow.getDate() + 1);
+console.log(dateNow.getDate() + 1)
 
-console.log(dateNow.getFullYear());
+console.log(dateNow.getFullYear())
 
-console.log(dateNow.getMonth());
+console.log(dateNow.getMonth())
 
 export function getDateNow() {
-  let dateNow = new Date();
+  let dateNow = new Date()
 
-  return dateNow;
+  let days=''
+  let month=''
+  if(dateNow.getDate()<10){
+    days='0'+dateNow.getDate()
+  }else{
+    days=dateNow.getDate()
+  }
+  if(dateNow.getMonth()+1<10){
+    month='0'+(dateNow.getMonth()+1)
+  }else{
+    month=dateNow.getMonth()+1
+  }
+
+  return `${days}/${month}/${dateNow.getFullYear()}`
+
 }
