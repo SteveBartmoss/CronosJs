8bfda2338bbb1d4b9b4b03e9291e09f12e25e7ed
Steve <za19012397@zapopan.tecmm.edu.mx>
2025-02-15 11:55:43 -0600

    feat: Se cambio la importacion de la funcion difference

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import { getDateNow, getDiference } from "./cronos/cronos.js";
+import { getDateNow, getDifference } from "./cronos/cronos.js";
 
 console.log(getDateNow('SQL'))
 
-console.log(getDiference('2023-10-01','2023-10-05'))
-console.log(getDiference('2023-10-05','2023-10-01'))
+console.log(getDifference('2023-10-01','2023-10-05'))
+console.log(getDifference('2023-10-05','2023-10-01'))
