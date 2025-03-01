7335b3b4ddc98a3534387cd35348bbc1144ef5f9
Steve <za19012397@zapopan.tecmm.edu.mx>
2025-03-01 10:52:16 -0600

    test: Se agregaron pruebas para la nueva implementacion de la funcion que formatea las fechas

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-import { getDateNow, getDifference } from "./cronos/cronos.js";
+import { getDateNow, getDifference, isValidDate } from "./cronos/cronos.js";
 
-console.log(getDateNow('SQL'))
+//console.log(getDateNow('SQL'))
 
-console.log(getDifference('2023-10-01','2023-10-05'))
-console.log(getDifference('2023-10-05','2023-10-01'))
+//console.log(getDifference('2023-10-01','2023-10-05'))
+//console.log(getDifference('2023-10-05','2023-10-01'))
+
+console.log(isValidDate("01-01-2025","DD-MM-YYYY"))
+console.log(isValidDate("01/01/2025","DD/MM/YYYY"))
+console.log(isValidDate("03-25-2025","MM-DD-YYYY"))
+console.log(isValidDate("03/25/2025","MM/DD/YYYY"))
+console.log(isValidDate("2025/01/01","YYYY/MM/DD"))
+console.log(isValidDate("2025-01-01","YYYY-MM-DD"))
