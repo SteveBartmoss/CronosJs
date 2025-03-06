8004e0478ac782990b158cb34d39099b0038f2fb
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2025-03-06 09:04:37 -0600

    feat: S agrego una funcion para convertiro unidades de tiempo

--- cronos/cronos.js
+++ cronos/cronos.js
@@ -387,3 +387,7 @@ export function hoursToSeconds(hours){
 export function hoursToMinutes(hours){
     return Math.floor(hours*60)
 }
+
+export function hoursToMiliseconds(hours){
+    return Math.floor(hours*60*60*1000)
+} 
