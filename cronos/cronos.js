7f9a0485def85a19562a074f6847e5383a480ad6
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2025-03-06 09:12:24 -0600

    feat: Se agrego una funcion para las conversiones de tiempo

--- cronos/cronos.js
+++ cronos/cronos.js
@@ -391,3 +391,7 @@ export function hoursToMinutes(hours){
 export function hoursToMiliseconds(hours){
     return Math.floor(hours*60*60*1000)
 } 
+
+export function milisecondsToHours(miliseconds){
+    return Math.floor(miliseconds/1000/60/60)
+}
