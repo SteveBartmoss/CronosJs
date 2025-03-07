3a7fc49b8e84e877743d649c19d655ba4b67ce60
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2025-03-07 08:53:20 -0600

    refactor: Se removio una funcion que ya se implemento de otra forma

--- cronos/cronos.js
+++ cronos/cronos.js
@@ -388,10 +388,6 @@ export function hoursToMinutes(hours){
     return Math.floor(hours*60)
 }
 
-export function hoursToMiliseconds(hours){
-    return Math.floor(hours*60*60*1000)
-} 
-
 export function unitTimeConvert(unit,typeOne,typeTwo){
 
     switch(typeOne){
