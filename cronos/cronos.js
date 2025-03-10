b0cbfcfb9194763c7f27b1ed4698c68666243291
steve <za19012397@zapopan.tecmm.edu.mx>
2025-03-09 19:38:30 -0600

    feat: Se limpio el codigo de una funcion que ahora esta implementada en el archivo de utlidades

--- cronos/cronos.js
+++ cronos/cronos.js
@@ -1,57 +1,6 @@
 //prototipo para normalizacion de las fechas
 
-import { formatUtility } from "./utils/generalUtils.js"
-
-function normalizeDate(date,format=null){
-
-    if(typeof date !== "string"){
-        throw new Error("Date must be a string")
-    }
-
-    const parts = date.split(/[-\/]/)
-
-    if(parts.length !== 3){
-        throw new Error('Invalid date format')
-    }
-
-    const formatMap = {
-        "DD/MM/YYYY": [2,1,0],
-        "MM/DD/YYYY": [2,0,1],
-        "YYYY/MM/DD": [0,1,2],
-        "DD-MM-YYYY": [2,1,0],
-        "MM-DD-YYYY": [2,0,1],
-        "YYYY-MM-DD": [0,1,2],
-        "YYYY-DD-MM": [0,2,1],
-    }
-
-    if (format in formatMap){
-        const [y,m,d] = formatMap[format].map(i => parts[i])
-        return `${y}-${m}-${d}`
-    }
-
-    let [a,b,c] = parts.map(Number)
-    let year, month, day
-
-    if(a>31){
-        year = a;
-        [month, day] = b > 12 ? [c,b] : [b,c]
-    } else if (c>31){
-        year = c
-        [month,day] = a > 12 ? [b,a] : [a,b]
-    }else{
-        year = c
-        [month,day] = [b,a]
-    }
-
-    const refactorDate = `${year}-${String(month).padStart(2,"0")}-${String(day).padStart(2,"0")}`
-
-    if (isNaN(Date.parse(refactorDate))) {
-        throw new Error("Invalid date")
-    }    
-
-    return refactorDate
-  
-}
+import { formatUtility, normalizeDate } from "./utils/generalUtils.js"
 
 function merge(array, startIndex, middleIndex, endIndex, format){
     const leftSize = middleIndex - startIndex + 1
