68919eb6b7f5629b33c179947e2881ba69b56880
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-06-17 08:35:23 -0600

    feat: Se implemento la funcion para ordenar un arrayDate

--- cronos/dateArray/dateArray.js
+++ cronos/dateArray/dateArray.js
@@ -1,3 +1,5 @@
+import { DateUtils } from "../utils/dateUtils"
+
 export class DateArray{
 
     constructor(listDates,format){
@@ -5,11 +7,62 @@ export class DateArray{
         this.format = format
     }
 
-    merge(){
+    merge(startIndex,middleIndex,endIndex){
         
+        const leftSize = middleIndex - startIndex + 1
+        const rightSize = endIndex - middleIndex
+
+        const leftSubarray = new Array(leftSize)
+        const rightSubarray = Array(rightSize)
+
+        for(let index = 0; index < rightSize; index++){
+            leftSubarray[index] = this.listDates[startIndex + index]
+        }
+
+        for(let index = 0; index < rightSize; index++){
+            rightSubarray[index] = this.listDates[middleIndex + 1 + index]
+        }
+
+        let leftIndex = 0, rightIndex = 0
+        let mergedIndex = startIndex
+
+        while (leftIndex < leftSize && rightIndex < rightSize){
+
+            const lefDate = new Date(DateUtils.normalizeDate(leftSubarray[leftIndex],this.format))
+            const rightDate = new Date(DateUtils.normalizeDate(rightSubarray[rightIndex],this.format))
+
+            if(lefDate <= rightDate){
+                this.listDates[mergedIndex] = leftSubarray[leftIndex]
+                leftIndex++
+            }
+            mergedIndex++
+        }
+
+        while(leftIndex < leftSize){
+            this.listDates[mergedIndex] = leftSubarray[leftIndex]
+            leftIndex++
+            mergedIndex++
+        }
+
+        while(rightIndex < rightSize){
+            this.listDates[mergedIndex] = rightSubarray[rightIndex]
+            rightIndex++
+            mergedIndex++
+        }
+
     }
 
-    mergeSortDate(startIndex,endIndex){
+    mergeSortDate(startIndex=0,endIndex=this.listDates.length){
 
+        if (startIndex >= endIndex){
+            return
+        }
+
+        const middleIndex = Math.floor(startIndex + (endIndex - startIndex)/2)
+
+        this.mergeSortDate(startIndex,middleIndex)
+        this.mergeSortDate(middleIndex, endIndex)
+        this.merge(startIndex, middleIndex, endIndex)
+        
     }
 }
