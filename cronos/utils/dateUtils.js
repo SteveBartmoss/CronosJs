0dda5513af52708a005d307f4eee5862bb6dedaf
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2025-06-24 16:38:21 -0600

    feat: Se implemento el primer camino para la normalizacion de las fechas

--- cronos/utils/dateUtils.js
+++ cronos/utils/dateUtils.js
@@ -13,15 +13,15 @@ export class DateUtils{
     }
 
     static formatMapTime = {
-        "h:mm:ss m": [],
-        "hh:mm m": [],
-        "hh:mm:ss" : [],
-        "hh:mm": [],
+        "h:mm:ss m": [0,1,2],
+        "hh:mm m": [0,1],
+        "hh:mm:ss" : [0,1,2],
+        "hh:mm": [0,1,2],
     }
 
     processDate(dateToProcess,format){
 
-        const parts = date.split(/[-\/]/)
+        const parts = dateToProcess.split(/[-\/]/)
 
         if(parts.length !== 3){
             throw new Error('Invalid date format')
@@ -54,10 +54,10 @@ export class DateUtils{
         }
 
         return refactorDate
-        
+
     }
 
-    processTime(){
+    processTime(timeToProcess,format){
 
     }
 
@@ -67,40 +67,16 @@ export class DateUtils{
             throw new Error("Date must be a string")
         }
 
-        const parts = date.split(/[-\/]/)
+        const formats = date.split(' ')
 
-        if(parts.length !== 3){
-            throw new Error('Invalid date format')
+        if(formats.length>0){
+
+            //let formatDate = processDate(date,)
+
+        } else {
+            return processDate(date,format)
         }
 
-        if(format in this.formatMap){
-            const [y,m,d] = this.formatMap[format].map(i => parts[i])
-
-            return `${y}-${m}-${d}`
-        }
-
-        let [a,b,c] = parts.map(Number)
-        let year, month, day
-
-        if(a>31){
-            year = a;
-            [month, day] = b > 12 ? [c,b] : [b,c]
-        } else if(c>31){
-            year = c
-            [month,day] = a > 12 ? [b,a] : [a,b]
-        }else{
-            year = c
-            [month,day] = [b,a]
-        }
-
-        const refactorDate = `${year}-${String(month).padStart(2,"0")}-${String(day).padStart(2,"0")}`
-
-        if(isNaN(Date.parse(refactorDate))){
-            throw new Error("Invalid date")
-        }
-
-        return refactorDate
-        
     }
 
     static formatDate(date,format){
