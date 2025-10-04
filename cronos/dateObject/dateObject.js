0c8d47152b1f0ad3b64f4fabddffe21fa6cfb6ff
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-10-04 14:31:49 -0600

    feat: Se magrego una funcion para obteber la diferente de la fecha del objeto con otra fecha

--- cronos/dateObject/dateObject.js
+++ cronos/dateObject/dateObject.js
@@ -7,12 +7,31 @@ export class DateObject{
         this.dateString = dateString
         this.format = format
         this.time = time
-        this.setDateJs
+        this.dateJs = new Date(DateUtils.normalizeDate(this.dateString,this.format))
     }
 
     setDateJs(){
         
         this.dateJs = new Date(DateUtils.normalizeDate(this.dateString,this.format))
+
+    }
+
+    getDifference(date,format){
+        if(date instanceof DateObject){
+            let difference = this.dateJs - date.dateJs
+            difference = difference / (1000 * 60 * 60 * 24)
+
+            return Math.round(difference)
+        }
+
+
+        let swapDate = new Date(DateUtils.normalizeDate(date,format))
+
+        let diference = this.dateJs - swapDate
+
+        diference = diference / (1000 * 60 * 60 * 24)
+
+        return Math.round(diference)
 
     }
 
